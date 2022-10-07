cf38fc99e38e84ccc231339bf9a580cee75482cf
Lubomír Blažek <lubomir.blazek@newlogic.cz>
2022-10-07 10:32:25 +0200

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config'
+import image from '@astrojs/image'
+
+export default defineConfig({
+    integrations: [image({
+        serviceEntryPoint: '@astrojs/image/sharp'
+    })]
+})
